ddc0074d011db439eca71e2e9a5d5140b4c0661f
Amrit <amrits@trainee.nrifintech.com>
2023-03-18 19:29:20 +0530

    changes in Doctor module-Frontend

--- frontend/src/main/resources/static/js/DoctorUpdateSchedule.js
+++ frontend/src/main/resources/static/js/DoctorUpdateSchedule.js
@@ -1,14 +1,9 @@
 function getWeekNumber(d) {
-    // Copy date so don't modify original
+    
     d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
-    // Set to nearest Thursday: current date + 4 - current day number
-    // Make Sunday's day number 7
     d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
-    // Get first day of year
     var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
-    // Calculate full weeks to nearest Thursday
     var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
-    // Return array of year and week number
     return [d.getUTCFullYear(), weekNo];
 }
 
@@ -21,35 +16,28 @@ function getDate(d) {
 var curDateTime=new Date()
 console.log(curDateTime.getDay())
 
-//console.log(curDateTime.getHours() + ":" + curDateTime.getMinutes() + ":" + curDateTime.getSeconds())
 
 var result = getWeekNumber(curDateTime);
 console.log(result)
 
 console.log(getDate(curDateTime))
 
-// .min = "2023-W16";
-// `${result[0]}-W${result[1]}`
+
 
 $(document).ready(function () {
 
-    var curDT;
+    
     var week_id =  document.getElementById("week")
     var start_time_id = document.getElementById("start_time")
     var end_time_id = document.getElementById("end_time")
-    var date_id = document.getElementById("date")
     
     week_id.min = `${result[0]}-W${result[1]}`
     week_id.value= `${result[0]}-W${result[1]}`
 
     start_time_id.value = (curDateTime.getHours()<10?'0':'') + curDateTime.getHours() + ":" + (curDateTime.getMinutes()<10?'0':'') + curDateTime.getMinutes()
     end_time_id.value = (curDateTime.getHours()<10?'0':'') + curDateTime.getHours() + ":" + (curDateTime.getMinutes()<10?'0':'') + curDateTime.getMinutes()
-    date_id.value=getDate(curDateTime)
+   
     
-    curDT=new Date(date.value)
-
-
-    var slotID=12;
 
     $.ajax({
         url: "http://localhost:8050/schedule/display/doctor/1",
@@ -59,17 +47,18 @@ $(document).ready(function () {
          
           $.each(result, function (key, value) {
             console.log(value);
-            // $(".add_doctor_personal").append("Name= " + value.name + "Speciality= " + value.speciality.name);
-            $("#schedule_table").append(`<tr>
-                        <td>${value.year}</td>
-                        <td>${value.week}</td>
-                        <td>${value.slotResponse.weekday}</td>
-                        <td>${value.slotResponse.start}</td>
-                        <td>${value.slotResponse.end}</td>
-                        <td>${value.slotResponse.capacity}</td>
-                        <td>${value.approval?"Approved":"Not Approved"}</td>
-                        <td><button id="remove_schedule" value=${value.id}>Remove</button></td>
-                      </tr>`);
+           
+            $(".responsive-table").append(`
+            <li class="table-row">
+            
+            <div class="col col-2" data-label="Date">${value.weekDate}</div>
+            <div class="col col-3" data-label="Day">${value.slotResponse.weekday}</div>
+            <div class="col col-4" data-label="StartTime">${value.slotResponse.start}</div>
+            <div class="col col-5" data-label="EndTime">${value.slotResponse.end}</div>
+            
+            <div class="col col-7" data-label="Status">${value.approval?"Approved":"Not Appproved"}</div>
+            <div class="col col-8" data-label="Remove"><button id="remove_button" type="remove" value=${value.id}>Remove</button></div>
+            </li>`);
           });
         },
         error: function (error) {
@@ -81,7 +70,7 @@ $(document).ready(function () {
     $("#save_button").click(function(e) {
         
         var slotData = {
-            "weekday": 2,
+            "weekday": $('#days').val(),
             "start": $('#start_time').val(),
             "end": $('#end_time').val()
         }
@@ -99,35 +88,46 @@ $(document).ready(function () {
             data: JSON.stringify(slotData),
             success: function (result) {
                 console.log(result);
-                
-                var scheduleData = {
-                    "doctorId": 1,
-                    "slotId": result.id,
-                    "weekDate": $('#week').val()
-                }
-                console.log(scheduleData)
+          
+                $.each(result, function (key, value) {
                 $.ajax({
                     url: "http://localhost:8050/schedule/",
                     type: "POST",
                     dataType: 'json',
                     contentType: 'application/json',
-                    data: JSON.stringify(scheduleData),
+                    data: JSON.stringify({
+                        "doctorId": 1,
+                        "slotId": value.id,
+                        "weekDate": $('#week').val()
+                    }),
                     success: function (result) {
                         console.log(result);
-                      window.location.href = 'UpdateSchedule';
+                      // window.location.href = 'UpdateSchedule';
                     },
                     error: function (error) {
                       console.log(error);
                     },
                 });
+              })
+              alert('Schedule Updated Successfully!')
+               window.location.href = 'UpdateSchedule';
             },
-            error: function (error) {
+            error: function (xhr, status, error) {
+              let errorObj;
               console.log(error);
+              console.log(xhr);
+              
+              if(xhr.responseText)
+              errorObj=JSON.parse(xhr.responseText);
+
+              if(errorObj) alert(errorObj.errorMessage)
+              else alert('Some Error Occurred')
+              //console.log(errorObj.errorMessage);
             },
         });
       });
 
-      $("#schedule_table").on("click","button#remove_schedule",function(e) {
+      $(".responsive-table").on("click","button#remove_button",function(e) {
         console.log("clicked")
         console.log(this)
         e.preventDefault();
@@ -135,6 +135,7 @@ $(document).ready(function () {
           type: "DELETE",
           url: `http://localhost:8050/schedule/${this.value}`,
           success: function(result) {
+            alert('Schedule Removed Successfully')
             window.location.href = 'UpdateSchedule';
           },
           error: function(result) {
