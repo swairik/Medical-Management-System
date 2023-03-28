1df0db6f460d2b6d15fe4a23ff431f39f12d9fd9
Sudipta Roy <sudiptar@trainee.nrifintech.com>
2023-03-28 16:56:33 +0530

    changes made in analytics

--- frontend/src/main/resources/static/js/AdminAnalytics.js
+++ frontend/src/main/resources/static/js/AdminAnalytics.js
@@ -1,4 +1,24 @@
+function formatDate(date) {
+  var d = new Date(date),
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
+
+  if (month.length < 2) 
+      month = '0' + month;
+  if (day.length < 2) 
+      day = '0' + day;
+
+  return [year, month, day].join('/');
+}
+
+
+
 $(document).ready(function () {
+
+  var curDateTime = new Date();
+  var today = formatDate(curDateTime);
+
     const cookie = document.cookie;
     if(cookie=='') window.location.href = "Auth";
     
@@ -34,29 +54,53 @@ $(document).ready(function () {
       },
     });
 
-    // $.ajax({
-    //   url: "http://localhost:8050/analytics/display/appointment/doctor",
-    //   type: "GET",
-    //   headers: {
-    //     Authorization: `Bearer ${token}`,
-    //   },
-    //   success: function (result) {
-    //     console.log(result);
-    //     $("#weekly_Appointments").html(result.Appointments);
-    //     $("#weekly_Doctors").html(result.Doctors);
-    //   },
-    //   error: function (xhr, status, errorThrown) {
-    //     if (xhr.status == 403) {
-    //       window.location.href = "Auth";
-    //     } else {
-    //       var errorObj;
-    //       if (xhr.responseText) errorObj = JSON.parse(xhr.responseText);
+    $.ajax({
+      url: "http://localhost:8050/analytics/display/appointment/monthly?stamp="+today,
+      type: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      success: function (result) {
+        console.log(result);
+        $("#Monthly_Appointments").html(result['Number of appointments in the current month']);
+        $("#Monthly_Doctors").html(result['Number of doctors with appointments in current month']);
+      },
+      error: function (xhr, status, errorThrown) {
+        if (xhr.status == 403) {
+          window.location.href = "Auth";
+        } else {
+          var errorObj;
+          if (xhr.responseText) errorObj = JSON.parse(xhr.responseText);
   
-    //       if (errorObj) alert(errorObj.errorMessage);
-    //       else alert("Some Error Occurred");
-    //     }
-    //   },
-    // });
+          if (errorObj) alert(errorObj.errorMessage);
+          else alert("Some Error Occurred");
+        }
+      },
+    });
+
+    $.ajax({
+      url: "http://localhost:8050/analytics/display/appointment/weekly?stamp="+today,
+      type: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      success: function (result) {
+        console.log(result);
+        $("#Weekly_Appointments").html(result['Number of appointments in the current week']);
+        $("#Weekly_Doctors").html(result['Number of doctors with appointments in current week']);
+      },
+      error: function (xhr, status, errorThrown) {
+        if (xhr.status == 403) {
+          window.location.href = "Auth";
+        } else {
+          var errorObj;
+          if (xhr.responseText) errorObj = JSON.parse(xhr.responseText);
+  
+          if (errorObj) alert(errorObj.errorMessage);
+          else alert("Some Error Occurred");
+        }
+      },
+    });
 
     $.ajax({
         url: "http://localhost:8050/analytics/display/specialityDoctorCount",
