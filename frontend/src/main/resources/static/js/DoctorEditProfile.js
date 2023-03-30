a6fc62756c5ee67521f2f0a342712c23f5c37a27
Amrit <amrits@trainee.nrifintech.com>
2023-03-30 16:23:57 +0530

    Admin-Update Doctor functionality

--- frontend/src/main/resources/static/js/DoctorEditProfile.js
+++ frontend/src/main/resources/static/js/DoctorEditProfile.js
@@ -1,91 +1,90 @@
 $(document).ready(function () {
-    const cookie = document.cookie;
-    if(cookie=='') window.location.href = "Auth";
-    
-    const token = cookie
-      .split("; ")
-      .find((row) => row.startsWith("authToken="))
-      .split("=")[1];
-    console.log(token);
+  const cookie = document.cookie;
+  if (cookie == "") window.location.href = "Auth";
 
-    const queryString = window.location.search;
+  const token = cookie
+    .split("; ")
+    .find((row) => row.startsWith("authToken="))
+    .split("=")[1];
+  console.log(token);
+
+  const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
 
   console.log(urlParams);
 
   const doctor_id = urlParams.get("doctor_id");
-   
-  
+
+  $.ajax({
+    url: `http://localhost:8050/doctor/display/${doctor_id}`,
+    type: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    success: function (result) {
+      console.log(result);
+      $("#dname").val(result.name);
+      $("#dage").val(result.age);
+      $("#dgender").val(result.gender);
+      $("#dphone").val(result.phone);
+      $("#demail").val(result.email);
+      $("#dspeciality").append(
+        `<option value=${result.speciality.id}>${result.speciality.name}</option>`
+      );
+    },
+    error: function (xhr, status, errorThrown) {
+      if (xhr.status == 403) {
+        window.location.href = "Auth";
+      } else {
+        alert("Some Error Occurred");
+      }
+    },
+  });
+
+  $(".pedit-form").on("submit", function (e) {
+    e.preventDefault();
+
+    console.log("clicked");
+
+    var doctorData = {
+      name: $("#dname").val(),
+      email: $("#demail").val(),
+      age: $("#dage").val(),
+      gender: $("#dgender").val(),
+      phone: $("#dphone").val(),
+      specialityId: $("#dspeciality").val(),
+    };
+    console.log(doctorData);
+
     $.ajax({
-      url: `http://localhost:8050/doctor/display/${doctor_id}`,
-      type: "GET",
+      url: `http://localhost:8050/doctor/${doctor_id}`,
+      method: "PUT",
+      dataType: "json",
+      contentType: "application/json",
+      data: JSON.stringify(doctorData),
       headers: {
         Authorization: `Bearer ${token}`,
       },
-      success: function (result) {
-        console.log(result);
-        $("#dname").val(result.name);
-        $("#dage").val(result.age);
-        $("#dgender").val(result.gender);
-        $("#dphone").val(result.phone);
-        $("#demail").val(result.email);
-        $("#dspeciality").append(`<option value=${result.speciality.id}>${result.speciality.name}</option>`);
+      success: function (response) {
+        // Handle successful response
+        console.log(response);
+        alert("Updated Successfully!");
+        window.location.href = "EditDoctor";
       },
       error: function (xhr, status, errorThrown) {
         if (xhr.status == 403) {
           window.location.href = "Auth";
         } else {
-          alert("Some Error Occurred");
+          var errorObj;
+
+          if (xhr.responseText) errorObj = JSON.parse(xhr.responseText);
+
+          console.log(errorObj);
+
+          if (errorObj) alert(errorObj.errorMessage);
+          else alert("Some Error Occurred");
         }
       },
     });
-  
-    // $(".pedit-form").on("submit", function (e) {
-    //   e.preventDefault();
-  
-    //   console.log("clicked");
-  
-    //   var patientData = {
-    //     name: $("#pname").val(),
-    //     email: $("#pemail").val(),
-    //     age: $("#page").val(),
-    //     gender: $("#pgender").val(),
-    //     phone: $("#pphone").val(),
-    //     email: $("#pemail").val(),
-    //   };
-    //   console.log(patientData);
-  
-    //   $.ajax({
-    //     url: `http://localhost:8050/patient/${patient_id}`,
-    //     method: "PUT",
-    //     dataType: "json",
-    //     contentType: "application/json",
-    //     data: JSON.stringify(patientData),
-    //     headers: {
-    //       Authorization: `Bearer ${token}`,
-    //     },
-    //     success: function (response) {
-    //       // Handle successful response
-    //       console.log(response);
-    //       alert("Updated Successfully!");
-    //       window.location.href = "PatientProfile";
-    //     },
-    //     error: function (xhr, status, errorThrown) {
-    //       if (xhr.status == 403) {
-    //         window.location.href = "Auth";
-    //       } else {
-    //         var errorObj;
-          
-    //         if (xhr.responseText) errorObj = JSON.parse(xhr.responseText);
-  
-    //         console.log(errorObj)
-  
-    //         if (errorObj) alert(errorObj.errorMessage);
-    //         else alert("Some Error Occurred");
-    //       }
-    //     },
-    //   });
-    // });
-
   });
-  
+});
