64360f3ac92af31cbfba2193c788707623f6e43d
swairikd <swairikd@trainee.nrifintech.com>
2023-04-05 11:03:32 +0530

    clean up

--- backend/src/main/java/com/mms/demo/serviceImpl/TokenServiceImpl.java
+++ backend/src/main/java/com/mms/demo/serviceImpl/TokenServiceImpl.java
@@ -86,6 +86,7 @@ public class TokenServiceImpl implements TokenService {
     @Transactional
     @Async
     public void tokenCleanerScheduler() {
+        System.out.println("Cleaning tokens");
         repository.deleteAllByExpirationStampBefore(LocalDateTime.now());
     }
 
